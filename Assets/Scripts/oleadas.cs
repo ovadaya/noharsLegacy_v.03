4369e9b2cba45d0eb59d68ce8f8e4bb4451a9c65
AlexCamos <119762645+AlexCamos@users.noreply.github.com>
2023-12-16 19:10:41 +0100

    cofre2

--- Assets/Scripts/oleadas.cs
+++ Assets/Scripts/oleadas.cs
@@ -9,8 +9,18 @@ public class oleadas : MonoBehaviour
     
     public GameObject[] sp;
 
+    private bool flag = true; 
+    private BoxCollider portalCollider;
+    public GameObject cofrePrefab;
+
     void Start()
     {
+        GameObject portal = GameObject.FindGameObjectWithTag("Portal");
+        if (portal != null)
+        {
+            portalCollider = portal.GetComponent<BoxCollider>();
+        }
+
         sp = GameObject.FindGameObjectsWithTag("Spawners");
         for(int i = 0; i < sp.Length; i++)
         {
@@ -18,27 +28,26 @@ public class oleadas : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    void Update()
     {
         if(GameObject.FindGameObjectsWithTag("MeleeEnemy").Length <= 0 && wave != wState.max)
         {
             sp[(int) wave].SetActive(true);
             wave++;
-
-           /*for(int i = 0; i < waves; i++){
-                Debug.Log("prueba " + i);
-                sp[i].SetActive(true);
-
-            }
-            waves++;*/
-            
         }
-        
-        //Debug.Log(GameObject.FindGameObjectsWithTag("MeleeEnemy").Length);
-        //Debug.Log(noenemies);
-        
-        
+        if (wave == wState.max && flag) 
+        {
+            generarCofre();
+            flag = false;
+            Debug.Log("Estado en max");
+        }
     }
 
-   
+    private void generarCofre()
+    {
+        portalCollider.isTrigger = true;
+        GameObject cofre = Instantiate(cofrePrefab);
+        cofre.transform.position = new Vector3(-270, 0.5f, 50);
+        flag = false;
+    }
 }
--- Assets/Scripts/spawn.cs
+++ Assets/Scripts/spawn.cs
@@ -7,46 +7,16 @@ public class spawn : MonoBehaviour
     [SerializeField] private int cantidade;
     [SerializeField] private GameObject enemytype;
 
-    private bool flag = true;
-    private BoxCollider portalCollider;
-    public GameObject cofrePrefab;
-
-    void Start()
-    {
-        GameObject portal = GameObject.FindGameObjectWithTag("Portal");
-        if (portal != null)
-        {
-            portalCollider = portal.GetComponent<BoxCollider>();
-        }
-    }
-
     void Update()
     {
         if (cantidade > 0) {
             spawnE();
             cantidade--;
         }
-        if(cantidade == 0)
-        {
-            if (flag) 
-            {
-                generarCofre();
-                flag = false;
-            } 
-        }
     }
 
     void spawnE()
     {
         GameObject enemyClone = Instantiate(enemytype, transform.position, Quaternion.identity);
-    }
-
-    private void generarCofre()
-    {
-        portalCollider.isTrigger = true;
-        GameObject cofre = Instantiate(cofrePrefab);
-        cofre.transform.position = new Vector3(-270, 0.5f, 50);
-        flag = false;
-        Debug.Log("Cofre generado");
     }
 }
