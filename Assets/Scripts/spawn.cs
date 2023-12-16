1377547458d2ad5b9cb6762d2516f2f3063b2d72
AlexCamos <119762645+AlexCamos@users.noreply.github.com>
2023-12-16 17:47:02 +0100

    cofre

--- Assets/Scripts/spawn.cs
+++ Assets/Scripts/spawn.cs
@@ -5,28 +5,48 @@ using UnityEngine;
 public class spawn : MonoBehaviour
 {
     [SerializeField] private int cantidade;
-
     [SerializeField] private GameObject enemytype;
-    // Start is called before the first frame update
+
+    private bool flag = true;
+    private BoxCollider portalCollider;
+    public GameObject cofrePrefab;
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(cantidade > 0){
-            spawnE();
-            cantidade--;
+        GameObject portal = GameObject.FindGameObjectWithTag("Portal");
+        if (portal != null)
+        {
+            portalCollider = portal.GetComponent<BoxCollider>();
         }
     }
 
-     void spawnE(){
-        
-            
-            GameObject enemyClone = Instantiate(enemytype, transform.position, Quaternion.identity);
-       
+    void Update()
+    {
+        if (cantidade > 0) {
+            spawnE();
+            cantidade--;
+        }
+        if(cantidade == 0)
+        {
+            if (flag) 
+            {
+                generarCofre();
+                flag = false;
+            } 
+        }
+    }
 
+    void spawnE()
+    {
+        GameObject enemyClone = Instantiate(enemytype, transform.position, Quaternion.identity);
+    }
+
+    private void generarCofre()
+    {
+        portalCollider.isTrigger = true;
+        GameObject cofre = Instantiate(cofrePrefab);
+        cofre.transform.position = new Vector3(-270, 0.5f, 50);
+        flag = false;
+        Debug.Log("Cofre generado");
     }
 }
