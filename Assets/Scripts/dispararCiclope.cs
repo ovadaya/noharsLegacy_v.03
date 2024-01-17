90775c670d1bb80e7a37e4ad63665068970b98e8
A-upv <amora.hcs.est@gmail.com>
2024-01-17 15:29:04 +0100

    dsfkslkf

---
+++ Assets/Scripts/dispararCiclope.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dispararCiclope : MonoBehaviour
+{
+    [SerializeField] private float timer = 5;
+
+    private float bullettime;
+
+    public Transform player;
+
+    public GameObject enemyBullet;
+
+    public Transform spawnPoint;
+
+    public float espeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        disparar();
+    }
+
+    void disparar(){
+        bullettime -= Time.deltaTime;
+
+        if(bullettime>0) return;
+
+        bullettime = timer;
+
+        GameObject bulletObj = Instantiate(enemyBullet, spawnPoint.transform.position, spawnPoint.transform.rotation) as GameObject;
+        Rigidbody rig = bulletObj.GetComponent<Rigidbody>();
+        rig.AddForce(rig.transform.forward * espeed);
+        Destroy(bulletObj, 5f);
+
+    }
+}
